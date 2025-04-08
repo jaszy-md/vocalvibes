c0f503494d452dbe35e5f71c50e866653517986b
Jaszy <jaszydh@gmail.com>
2025-04-09 00:36:30 +0200

    header + banner

--- functions.php
+++ functions.php
@@ -48,3 +48,49 @@ add_action('wp_enqueue_scripts', 'vocalvibes_scripts');
 
 // ✅ Laad de custom template tags, zoals vocalvibes_posted_on()
 require get_template_directory() . '/inc/template-tags.php';
+
+
+// ✅ Herbruikbare banner op basis van ACF
+// Aangepaste versie:
+function vocalvibes_banner($args = [])
+{
+	$banner_type = get_field('banner_type');
+	$banner_afbeelding = get_field('banner_afbeelding');
+	$banner_video = get_field('banner_video_url');
+	$titel_afbeelding = get_field('titel_afbeelding');
+
+	// Fallbacks
+	$fallback_banner_image = get_theme_file_uri('/assets/images/default-banner.png');
+	$fallback_titel_afbeelding = get_theme_file_uri('/assets/images/ribbon.png');
+
+	$title = isset($args['title']) ? $args['title'] : get_the_title();
+	$subtitle = isset($args['subtitle']) ? $args['subtitle'] : get_field('banner_subtitle');
+
+	// Altijd een afbeelding instellen (fallback als niets is gekozen)
+	if ($banner_type === 'video' && $banner_video) {
+		$media = '<video autoplay muted loop playsinline class="banner__video"><source src="' . esc_url($banner_video) . '" type="video/mp4"></video>';
+	} elseif ($banner_type === 'image' && $banner_afbeelding) {
+		$image_url = esc_url($banner_afbeelding['url']);
+		$media = '<div class="banner__image" style="background-image: url(' . $image_url . ');"></div>';
+	} else {
+		$media = '<div class="banner__image" style="background-image: url(' . esc_url($fallback_banner_image) . ');"></div>';
+	}
+
+	$titel_afbeelding_url = $titel_afbeelding ? esc_url($titel_afbeelding['url']) : $fallback_titel_afbeelding;
+?>
+	<section class="banner">
+		<?php echo $media; ?>
+
+		<div class="banner__title-image-wrapper">
+			<img src="<?php echo $titel_afbeelding_url; ?>" alt="Titel afbeelding" class="banner__title-image" />
+			<h1 class="banner__title-on-image"><?php echo esc_html($title); ?></h1>
+		</div>
+
+		<?php if ($subtitle) : ?>
+			<div class="banner__content container">
+				<p class="banner__subtitle"><?php echo esc_html($subtitle); ?></p>
+			</div>
+		<?php endif; ?>
+	</section>
+<?php
+}
--- header.php
+++ header.php
@@ -16,7 +16,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<!-- Material Icons -->
 	<link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
@@ -30,33 +29,35 @@
 
 		<header class="header">
 			<div class="header__topbar">
-				<div class="header__topbar-left"></div>
+				<div class="wrapper">
+					<div class="header__topbar-left"></div>
 
-				<div class="header__promo">Boek nu je eerste GRATIS proefles!</div>
+					<div class="header__promo">Boek nu je eerste GRATIS proefles!</div>
 
-				<div class="header__topbar-icons">
-					<a href="mailto:info@vocalvibes.nl" title="Mail ons">
-						<span class="material-icons-outlined">mail_outline</span>
-					</a>
-					<a href="tel:0612345678" title="Bel ons">
-						<span class="material-icons-outlined">call</span>
-					</a>
+					<div class="header__topbar-icons">
+						<a href="mailto:info@vocalvibes.nl" title="Mail ons">
+							<span class="material-icons-outlined">mail_outline</span>
+						</a>
+						<a href="tel:0612345678" title="Bel ons">
+							<span class="material-icons-outlined">call</span>
+						</a>
+					</div>
 				</div>
 			</div>
-
-			<div class="header__inner">
-				<div class="header__logo">
-					<?php the_custom_logo(); ?>
+			<div class="wrapper">
+				<div class="header__inner">
+					<div class="header__logo">
+						<?php the_custom_logo(); ?>
+					</div>
+					<nav class="header__nav">
+						<?php
+						wp_nav_menu(array(
+							'theme_location' => 'headerMenuLocation',
+							'menu_class'     => 'header__menu',
+							'container'      => false,
+						));
+						?>
+					</nav>
 				</div>
-
-				<nav class="header__nav">
-					<?php
-					wp_nav_menu(array(
-						'theme_location' => 'headerMenuLocation',
-						'menu_class'     => 'header__menu',
-						'container'      => false,
-					));
-					?>
-				</nav>
 			</div>
 		</header>
