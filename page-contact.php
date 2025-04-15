d54f7f52dda58c7ec2e57f963ce8394135f9bb68
Jaszy <jaszydh@gmail.com>
2025-04-15 22:12:24 +0200

    home done

--- page-contact.php
+++ page-contact.php
@@ -11,7 +11,6 @@ vocalvibes_banner();
 <main class="contactpage">
     <section class="contact-hero">
         <h1>Neem contact op</h1>
-        <!-- Voeg hier je contactformulier en info toe -->
     </section>
     <?php get_template_part('template-parts/youtube-section'); ?>
 
